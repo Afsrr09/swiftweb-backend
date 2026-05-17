b23fbe5452b392b05171b4665b45bff42dec2b94
Anarul islam <developeranarul@gmail.com>
2026-05-18 02:09:08 +0600

    fix railway mysql

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -19,12 +19,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const mySqlDB = await mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-export { mySqlDB };
+try {
+  const connection = await pool.getConnection();
+  console.log("✅ MySQL Connected");
+  connection.release();
+} catch (error) {
+  console.error("❌ Database Error:", error.message);
+}
+
+export default pool;
