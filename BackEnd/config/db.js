87d40f2f28d26c9d9a5b5deeb8f5855e3371c43d
Anarul Islam <155163786+Afsrr09@users.noreply.github.com>
2026-05-19 14:32:32 +0600

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,15 +1,13 @@
 
 
-import dotenv from "dotenv";
-dotenv.config();
 import mysql from "mysql2/promise";
 
-//to connect to mysql server
 const mySqlDB = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
-export { mySqlDB};
+
+export { mySqlDB };
