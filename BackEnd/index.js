d30623fd3807d01ee36e1510c1b8a84b327bbbe7
Anarul Islam <155163786+Afsrr09@users.noreply.github.com>
2026-05-19 21:01:12 +0600

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -24,8 +24,7 @@ const PORT = process.env.PORT || 3000;
 
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 app.use(cookieParser());
 app.use(express.json());
