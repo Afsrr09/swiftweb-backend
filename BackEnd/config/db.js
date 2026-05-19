a3d1dd4dbc41036805927414064d4232620a97bb
Anarul Islam <155163786+Afsrr09@users.noreply.github.com>
2026-05-19 17:47:36 +0600

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,15 +1,27 @@
 
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
+
+// const mySqlDB = await mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
+// export { mySqlDB };
+
+import mysql from "mysql2/promise";
 
 const mySqlDB = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 export { mySqlDB };
